30fe93027e8e01abec1bbfd5ba2d56e863b57194
XYGENykt <zee@labis.su>
2025-05-08 10:47:16 +0900

    14

--- arraySlice/ex14.go
+++ arraySlice/ex14.go
@@ -1,3 +1,38 @@
 // Дан список, упорядоченный по не убыванию элементов в нем. Определите, сколько в нем различных элементов.
 // Если все элементы массива одинаковы, то количество различных элементов равняется 1.
 
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		n int
+	)
+
+	fmt.Scan(&n)
+
+	array := make([]int, n)
+
+	for i := range array {
+		fmt.Scan(&array[i])
+	}
+
+	count := 0
+
+	if n > 1 {
+		count++
+		for i := 0; i < n-1; i++ {
+			if array[i] != array[i+1] {
+				count++
+			}
+
+		}
+
+	}
+	if n == 1 {
+		count++
+	}
+
+	fmt.Print(count)
+}
