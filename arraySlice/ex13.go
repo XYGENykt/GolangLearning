1f9d547961dda888655793fe80f0936bd52121f3
XYGENykt <zee@labis.su>
2025-05-08 09:25:36 +0900

    13

--- arraySlice/ex13.go
+++ arraySlice/ex13.go
@@ -1,3 +1,37 @@
 // Дан список положительных целых чисел. Найдите все числа кратные 3 и при этом оканчивающиеся на 7,
 // и замените каждое из этих чисел на общее коллчество таких чисел в массиве.
 
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		n int
+	)
+	fmt.Scan(&n)
+	array := make([]int, n)
+
+	for i := range array {
+		fmt.Scan(&array[i])
+	}
+
+	count := 0
+
+	for i := range array {
+		if array[i]%3 == 0 && array[i]%10 == 7 {
+			count++
+		}
+	}
+
+	for i := range array {
+		if array[i]%3 == 0 && array[i]%10 == 7 {
+			array[i] = count
+		}
+	}
+
+	for i := range array {
+		fmt.Print(array[i], " ")
+	}
+
+}
